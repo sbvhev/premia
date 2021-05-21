0d807be76bd2d99c0ebd445aa932c7a2fbc90a93
sbvhev <rohannepal200@gmail.com>
2021-05-20 21:16:00 -0400

    Add layout for pro vault

--- src/pages/ProVault/ProVault.tsx
+++ src/pages/ProVault/ProVault.tsx
@@ -6,26 +6,46 @@ import {
   BottomNavigation,
   BottomNavigationAction,
   useMediaQuery,
+  Paper,
 } from '@material-ui/core';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
+import cn from 'classnames';
+import { makeStyles, useTheme, Theme } from '@material-ui/core/styles';
 import { LineChart, RadialChart } from 'components';
 import { ReactComponent as BasicIcon } from 'assets/svg/BasicIcon.svg';
 import { ReactComponent as ProIcon } from 'assets/svg/ProIcon.svg';
 import { ReactComponent as UniswapIcon } from 'assets/svg/Uniswap.svg';
 import { ReactComponent as DaiIcon } from 'assets/svg/Dai.svg';
+import { ArrowUpward, ArrowDownward } from '@material-ui/icons';
 
 import { useWeb3 } from 'state/application/hooks';
-import { shortenAddress } from 'utils';
-
 import { PageWithSidebar } from 'layouts';
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles((theme: Theme) => ({
   title: {
     fontWeight: 700,
     fontSize: '28px',
     lineHeight: '27.5px',
-    marginBottom: 36
+    marginBottom: 36,
   },
+  topTab: {
+    marginBottom: 20,
+  },
+  mainContent: {},
+  call: {
+  },
+  put: {
+  },
+  header: {
+    display: 'flex',
+    marginBottom: 20
+  },
+  topSector: {
+    padding: 28,
+    border: `1px solid ${theme.palette.divider}`
+  },
+  bottomSector: {
+    padding: 28
+  }
 }));
 
 const Stake: React.FC = () => {
@@ -38,9 +58,7 @@ const Stake: React.FC = () => {
   return (
     <PageWithSidebar>
       <Grid container direction='column'>
-        <Box
-          width={1}
-        >
+        <Box width={1}>
           <Typography
             component='h1'
             variant='h3'
@@ -49,16 +67,54 @@ const Stake: React.FC = () => {
           >
             Vaults
           </Typography>
-          <BottomNavigation
-            value={value}
-            onChange={(event, newValue) => {
-              setValue(newValue);
-            }}
-            showLabels={true}
-          >
-            <BottomNavigationAction label='Basic' icon={<BasicIcon />} />
-            <BottomNavigationAction label='Pro' icon={<ProIcon />} />
-          </BottomNavigation>
+          <Grid container direction='row' className={classes.topTab}>
+            <BottomNavigation
+              value={value}
+              onChange={(event, newValue) => {
+                setValue(newValue);
+              }}
+              showLabels={true}
+            >
+              <BottomNavigationAction label='Basic' icon={<BasicIcon />} />
+              <BottomNavigationAction label='Pro' icon={<ProIcon />} />
+            </BottomNavigation>
+          </Grid>
+          <Grid container direction='row'>
+            <Grid item xs={6}>
+              <Paper className={classes.call}>
+                <Box component="div" className={classes.topSector}>
+                  <Box component="div" className={classes.header}>
+                    <ArrowUpward />
+                    <Typography
+                      variant="body1"
+                      component="h2"
+                      color="textPrimary"
+                    >
+                      Call Pool
+                    </Typography>
+                    <Typography
+                      variant="body2"
+                      component="h2"
+                      color="textPrimary"
+                    >
+                      78% Utilization
+                    </Typography>
+                  </Box>
+                  <Box component="div">
+                    <RadialChart />
+                  </Box>
+                </Box>
+                <Box component="div" className={classes.bottomSector}>
+
+                </Box>
+              </Paper>
+            </Grid>
+            <Grid item xs={6}>
+              <Paper className={classes.put}>
+                <ArrowDownward />
+              </Paper>
+            </Grid>
+          </Grid>
         </Box>
       </Grid>
     </PageWithSidebar>
