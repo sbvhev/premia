af9bc1a0eeb345825e7b6f030418efbfdcced5d7
sbvhev <rohannepal200@gmail.com>
2021-05-21 10:06:56 -0400

    Basic Vault page

--- src/pages/Vault/BasicVault.tsx
+++ src/pages/Vault/BasicVault.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import cn from 'classnames';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
 
 import {
@@ -10,27 +11,27 @@ import {
   InputLabel,
   MenuItem,
   useMediaQuery,
+  Grid,
+  Paper,
 } from '@material-ui/core';
 import { ReactComponent as UniswapIcon } from 'assets/svg/Uniswap.svg';
 import { ReactComponent as WBTCIcon } from 'assets/svg/wBTCIcon.svg';
 import { ReactComponent as ETHIcon } from 'assets/svg/ETHIcon.svg';
 import { ReactComponent as YFIIcon } from 'assets/svg/YFIIcon.svg';
 import { ReactComponent as LinkIcon } from 'assets/svg/LinkIcon.svg';
-import { ExpandMore } from '@material-ui/icons';
+import { ReactComponent as VaultBlueIcon } from 'assets/svg/VaultBlue.svg';
+import { ReactComponent as VaultGreenIcon } from 'assets/svg/VaultGreen.svg';
+import { ReactComponent as VaultRedIcon } from 'assets/svg/VaultRed.svg';
+import { ExpandMore, Check } from '@material-ui/icons';
 import { findLastIndex } from 'lodash';
 
 const useStyles = makeStyles(({ palette }) => ({
-  wrapper: {
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'flex-start',
-    backgroundcolor: 'transparent',
-  },
   borderedCard: {
     alignSelf: 'flex-start',
     flexDirection: 'column',
     justifyContent: 'flex-start',
     padding: 28,
+    marginRight: 32,
     width: 340,
     border: `1px solid ${palette.divider}`,
     backgroundColor: palette.background.default,
@@ -39,7 +40,7 @@ const useStyles = makeStyles(({ palette }) => ({
   titleBox: {
     display: 'flex',
     flexDirection: 'column',
-    alignItems: 'center',
+    alignItems: 'start',
     textAlign: 'start',
   },
   title: {
@@ -51,7 +52,7 @@ const useStyles = makeStyles(({ palette }) => ({
     fontWeight: 500,
     fontSize: '12px',
     lineHeight: '24px',
-    paddingLeft: 8
+    paddingLeft: 8,
   },
   col: {
     boxSizing: 'border-box',
@@ -120,6 +121,118 @@ const useStyles = makeStyles(({ palette }) => ({
       height: 16,
     },
   },
+  rightCard: {
+    paddingTop: 32,
+    width: 'calc(100% - 460px)',
+
+    '& $title': {
+      marginBottom: 12,
+      height: 20,
+    },
+  },
+  vaultCard: {
+    height: '100%',
+    borderRadius: 12,
+    background: 'rgba(82, 148, 255, 0.1)',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'column',
+    cursor: 'pointer',
+    position: 'relative',
+    zIndex: 2,
+
+    '& svg': {
+      width: 90,
+      height: 90,
+      marginBottom: 18,
+      zIndex: 4,
+    },
+
+    '& h2': {
+      zIndex: 4,
+    },
+
+    '&.selected': {
+      '&:before': {
+        content: '""',
+        position: 'absolute',
+        top: 0,
+        right: 0,
+        bottom: 0,
+        left: 0,
+        zIndex: 1,
+        margin: -2,
+        borderRadius: 'inherit',
+        background: `linear-gradient(to right, #5294FF, #1EFF78)`,
+      },
+
+      '& $background': {
+        zIndex: 0
+      },
+
+      '& $select': {
+        zIndex: 4,
+        background: 'black'
+      }
+    },
+
+    '&:hover': {
+      '&:before': {
+        content: '""',
+        position: 'absolute',
+        top: 0,
+        right: 0,
+        bottom: 0,
+        left: 0,
+        zIndex: 1,
+        margin: -2,
+        borderRadius: 'inherit',
+        background: `linear-gradient(to right, #5294FF, #1EFF78)`,
+      },
+
+      '& $select': {
+        zIndex: 4,
+      },
+    },
+  },
+  select: {
+    border: '1px solid rgba(255, 255, 255, 0.1)',
+    color: 'white',
+    fontSize: 14,
+    lineHeight: '18px',
+    borderRadius: 9,
+    height: 40,
+    width: 140,
+    display: 'flex',
+    zIndex: -1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 12,
+    cursor: 'pointer',
+
+    '& svg': {
+      width: 16,
+      height: 16,
+      marginRight: 8,
+      marginBottom: 0
+    },
+
+    '&:hover': {
+      background: 'black',
+      border: 'transparent',
+    },
+  },
+  background: {
+    background: '#080f19',
+    borderRadius: 12,
+    zIndex: 3,
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '100%',
+  },
 }));
 
 const BasicVault: React.FC = () => {
@@ -127,6 +240,7 @@ const BasicVault: React.FC = () => {
   const theme = useTheme();
   const mobile = useMediaQuery(theme.breakpoints.down('sm'));
   const [coin, setCoin] = useState<any>(null);
+  const [selectedIndex, setSelectedIndex] = useState(0);
 
   const handleChange = (
     event: React.ChangeEvent<{ name?: string | undefined; value: unknown }>,
@@ -136,10 +250,8 @@ const BasicVault: React.FC = () => {
   };
 
   return (
-    <Box className={classes.wrapper}>
-      <Box
-        className={classes.borderedCard}
-      >
+    <Grid container direction='row'>
+      <Box className={classes.borderedCard}>
         <Box className={classes.titleBox}>
           <Box>
             <Typography
@@ -151,9 +263,7 @@ const BasicVault: React.FC = () => {
             </Typography>
           </Box>
         </Box>
-        <Box
-          className={classes.topSection}
-        >
+        <Box className={classes.topSection}>
           <Box className={classes.col}>
             <Box
               display='flex'
@@ -247,10 +357,10 @@ const BasicVault: React.FC = () => {
               </Button>
             </Box>
             <Typography
-                component='p'
-                color='textSecondary'
-                className={classes.smallInfoText}
-              >
+              component='p'
+              color='textSecondary'
+              className={classes.smallInfoText}
+            >
               {'Max size available: 8,912'}
             </Typography>
           </Box>
@@ -262,7 +372,117 @@ const BasicVault: React.FC = () => {
           </Box>
         </Box>
       </Box>
-    </Box>
+      <Box className={classes.rightCard}>
+        <Grid container style={{ height: '100%' }}>
+          <Typography
+            component='h2'
+            color='textPrimary'
+            className={classes.title}
+          >
+            Select vault
+          </Typography>
+          <Grid
+            container
+            direction='row'
+            style={{ height: 'calc(100% - 18px)' }}
+            spacing={2}
+          >
+            <Grid item xs={4}>
+              <Box
+                className={cn(
+                  classes.vaultCard,
+                  selectedIndex === 1 ? 'selected' : '',
+                )}
+              >
+                <Box className={classes.background} />
+                <VaultBlueIcon />
+                <Typography
+                  component='h2'
+                  color='textSecondary'
+                  variant='body2'
+                >
+                  Low risk
+                </Typography>
+                <Typography component='h2' color='textPrimary'>
+                  10% Expected APY
+                </Typography>
+                <Box
+                  className={classes.select}
+                  onClick={() => {
+                    setSelectedIndex(1);
+                  }}
+                >
+                  { selectedIndex === 1 && <Check />}
+                  { selectedIndex === 1 ? 'Selected' : 'Select' }
+                </Box>
+              </Box>
+            </Grid>
+            <Grid item xs={4}>
+              <Box
+                className={cn(
+                  classes.vaultCard,
+                  selectedIndex === 2 ? 'selected' : '',
+                )}
+              >
+                <Box
+                  className={classes.background}
+                />
+                <VaultGreenIcon />
+                <Typography
+                  component='h2'
+                  color='textSecondary'
+                  variant='body2'
+                >
+                  Medium risk
+                </Typography>
+                <Typography component='h2' color='textPrimary'>
+                  20% Expected APY
+                </Typography>
+                <Box
+                  className={classes.select}
+                  onClick={() => {
+                    setSelectedIndex(2);
+                  }}
+                >
+                  { selectedIndex === 2 && <Check />}
+                  { selectedIndex === 2 ? 'Selected' : 'Select' }
+                </Box>
+              </Box>
+            </Grid>
+            <Grid item xs={4}>
+              <Box
+                className={cn(
+                  classes.vaultCard,
+                  selectedIndex === 3 ? 'selected' : '',
+                )}
+              >
+                <Box className={classes.background} />
+                <VaultRedIcon />
+                <Typography
+                  component='h2'
+                  color='textSecondary'
+                  variant='body2'
+                >
+                  High risk
+                </Typography>
+                <Typography component='h2' color='textPrimary'>
+                  60% Expected APY
+                </Typography>
+                <Box
+                  className={classes.select}
+                  onClick={() => {
+                    setSelectedIndex(3);
+                  }}
+                >
+                  { selectedIndex === 3 && <Check />}
+                  { selectedIndex === 3 ? 'Selected' : 'Select' }
+                </Box>
+              </Box>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Box>
+    </Grid>
   );
 };
 
