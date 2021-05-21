4cbda32695b0af9d87213418220571ff28713d22
RoderickJS <rod@infinitegame.co>
2021-05-20 19:37:37 -0500

    fix linear charts and switchTab to use theme without dark/light props

--- src/components/SwitchTab/SwitchTab.tsx
+++ src/components/SwitchTab/SwitchTab.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import cn from 'classnames';
-import { makeStyles, Theme } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import { BottomNavigation } from '@material-ui/core';
 
 export interface SwitchProps {
@@ -12,19 +11,19 @@ export interface SwitchProps {
   onChange: (event: React.ChangeEvent<{}>, newValue: number) => void;
 }
 
-const useStyles = makeStyles((theme: Theme) => ({
-  light: {
-    border: 'none',
-    background: 'white',
-    boxShadow: '0px 2px 5px rgba(0, 0, 0, 0.07)',
+const useStyles = makeStyles(({ palette }) => ({
+  root: {
+    border: `1px solid ${palette.divider}`,
+    background: palette.background.default,
+    // boxShadow: '0px 2px 5px rgba(0, 0, 0, 0.07)',
   },
 }));
 
-const SwitchTab: React.FC<SwitchProps> = ({ dark, children, className, ...props}) => {
+const SwitchTab: React.FC<SwitchProps> = ({ children, ...props}) => {
   const classes = useStyles();
 
   return (
-    <BottomNavigation className={cn(dark ? '' : classes.light, className)} {...props}>
+    <BottomNavigation className={classes.root} {...props}>
       {children}
     </BottomNavigation>
   );
--- src/pages/ProVault/ProVault.tsx
+++ src/pages/ProVault/ProVault.tsx
@@ -4,9 +4,7 @@ import {
   Grid,
   Typography,
   BottomNavigationAction,
-  useMediaQuery,
 } from '@material-ui/core';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
 import { SwitchTab, LineChart, RadialChart } from 'components';
 import RestoreIcon from '@material-ui/icons/Restore';
 import FavoriteIcon from '@material-ui/icons/Favorite';
@@ -14,132 +12,17 @@ import LocationOnIcon from '@material-ui/icons/LocationOn';
 import { ReactComponent as UniswapIcon } from 'assets/svg/Uniswap.svg';
 import { ReactComponent as DaiIcon } from 'assets/svg/Dai.svg';
 
-import { useWeb3 } from 'state/application/hooks';
-import { shortenAddress } from 'utils';
 
 import { PageWithSidebar } from 'layouts';
 
-const useStyles = makeStyles(() => ({
-  topContainer: {},
-
-  title: {
-    whiteSpace: 'nowrap',
-  },
-
-  titleMobile: {
-    fontSize: '3.1vw',
-  },
-
-  smallerTitle: {
-    whiteSpace: 'nowrap',
-    fontSize: '1.5vw',
-  },
-
-  smallerTitleMobile: {
-    whiteSpace: 'nowrap',
-    fontSize: '2vw',
-  },
-
-  subtitle: {
-    fontWeight: 300,
-  },
-
-  topContainerMobile: {
-    margin: '80px 0 0 12px',
-    width: 'calc(100% - 50px)',
-  },
-}));
-
-const Stake: React.FC = () => {
-  const { account, wallet } = useWeb3();
-  const classes = useStyles();
-  const theme = useTheme();
-  const mobile = useMediaQuery(theme.breakpoints.down('sm'));
+const Vault: React.FC = () => {
   const [value, setValue] = useState(0);
 
   return (
     <PageWithSidebar>
       <Grid container direction='column'>
-        <Box
-          width={1}
-          className={mobile ? classes.topContainerMobile : classes.topContainer}
-        >
-          {wallet && wallet.provider && account ? (
-            <>
-              <Typography
-                component='h2'
-                variant='h4'
-                color='textPrimary'
-                style={{
-                  fontFamily: '"Inter"',
-                  fontWeight: 500,
-                  fontSize: 24,
-                  lineHeight: 1.5,
-                }}
-              >
-                Hi {shortenAddress(account ?? '')},
-              </Typography>
-
-              <Typography
-                component='h2'
-                variant='h3'
-                color='textPrimary'
-                style={
-                  !mobile
-                    ? {
-                        fontFamily: '"Teko"',
-                        fontWeight: 600,
-                        fontSize: 48,
-                        marginBottom: 32,
-                      }
-                    : {
-                        fontFamily: '"Teko"',
-                        fontWeight: 600,
-                        fontSize: 48,
-                      }
-                }
-              >
-                Welcome to Pro Vaults
-              </Typography>
-            </>
-          ) : (
-            <Typography
-              component='h2'
-              variant='h3'
-              color='textPrimary'
-              style={
-                !mobile
-                  ? {
-                      fontFamily: '"Teko"',
-                      fontWeight: 600,
-                      fontSize: 48,
-                      marginBottom: 32,
-                    }
-                  : {
-                      fontFamily: '"Teko"',
-                      fontWeight: 600,
-                      fontSize: 40,
-                      marginBottom: 8,
-                    }
-              }
-            >
-              Pro Vaults page
-            </Typography>
-          )}
+        <Box>
           <SwitchTab
-            dark={true}
-            value={value}
-            onChange={(event, newValue) => {
-              setValue(newValue);
-            }}
-            showLabels={true}
-          >
-            <BottomNavigationAction label='Recents' icon={<RestoreIcon />} />
-            <BottomNavigationAction label='Favorites' icon={<FavoriteIcon />} />
-            <BottomNavigationAction label='Nearby' icon={<LocationOnIcon />} />
-          </SwitchTab>
-          <SwitchTab
-            dark={false}
             value={value}
             onChange={(event, newValue) => {
               setValue(newValue);
@@ -152,7 +35,6 @@ const Stake: React.FC = () => {
           </SwitchTab>
           <LineChart
             color='#BF47C3'
-            dark={true}
             data={[2345, 3423, 3323, 2643, 3234, 6432, 1234]}
             categories={['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']}
             width={500}
@@ -160,24 +42,7 @@ const Stake: React.FC = () => {
           />
           <LineChart
             color='#14A887'
-            dark={true}
-            data={[2345, 3423, 3323, 2643, 3234, 6432, 1234]}
-            categories={['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']}
-            width={500}
-            height={200}
-          />
-          <LineChart
-            color='#BF47C3'
-            dark={false}
-            data={[2345, 3423, 3323, 2643, 3234, 6432, 1234]}
-            categories={['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']}
-            width={500}
-            height={200}
-          />
-          <LineChart
-            color='#14A887'
-            dark={false}
-            data={[2345, 3423, 3323, 2643, 3234, 6432, 1234]}
+            data={[2345, 3423, 3323, 2643, 3234, 6432, 1234, 2345, 3423, 3323, 2643, 3234, 6432, 1234]}
             categories={['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']}
             width={500}
             height={200}
@@ -212,4 +77,4 @@ const Stake: React.FC = () => {
   );
 };
 
-export default Stake;
+export default Vault;
