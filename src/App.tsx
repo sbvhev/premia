0dd2704b0e3699016278543e32aec87ddcc29c0a
Froggie Dev <froggiedev@protonmail.com>
2021-05-21 17:08:48 -0700

    Add global css

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,9 @@ import {
   CssBaseline,
 } from '@material-ui/core';
 
+import 'react-dates/initialize';
+import 'react-dates/lib/css/_datepicker.css';
+
 import {
   useModalOpen,
   useCloseModals,
