bf6ddd888d56822159352bb58042000e4756632e
sbvhev <rohannepal200@gmail.com>
2021-05-28 15:53:05 -0400

    Search tab border fix

--- src/components/SearchTabs/SearchTabs.tsx
+++ src/components/SearchTabs/SearchTabs.tsx
@@ -31,6 +31,8 @@ const useStyles = makeStyles((theme: Theme) => ({
     '& .MuiOutlinedInput-root': {
       paddingRight: 0,
       height: 42,
+      borderColor: (props: any) =>
+        props.dark ? 'transparent' : theme.palette.divider,
 
       '& input': {
         '&:focus::placeholder': {
