0d19755a7878e30f2ce68012fd1ce5e45083e566
froggiedev <froggiedev@protonmail.com>
2021-06-13 09:26:03 -1000

    Remove unnecessary comments

--- src/utils/roundToLog.ts
+++ src/utils/roundToLog.ts
@@ -5,11 +5,6 @@ export function roundToLog(
 ): number {
   const rounding =
     Math.pow(10, Math.ceil(Math.log10(value)) + plusX) * multiplier;
-
-  console.log('step 1:', Math.ceil(Math.log10(value)));
-  console.log('step 2:', rounding);
-  console.log('final: ', Math.round(value / rounding) * rounding);
-
   return Math.round(value / rounding) * rounding;
 }
 
